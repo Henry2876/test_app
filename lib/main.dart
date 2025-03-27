3628ee1dcba8c678cf3ef6884bf14db8b0709900
Henry2876 <henrywwwalker@gmail.com>
2025-03-27 19:54:43 +0300

    assignment 5 v:1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,446 @@
-// Задание №5
+// Задание 6
+
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp (const MyApp());
-
+void main()=>runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Three Button",
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title:const Text('___Button___'),
-            centerTitle: true,
-            titleTextStyle:const TextStyle(fontSize: 30,color:  Colors.blue),
-          ),
-
-          body:Column(children: [
-
-            Container (
-                margin:const EdgeInsets.all(16),
-                height: 50,
-                width: 400,
-
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.white.withOpacity(0.08),
-                        offset:const Offset(0, 4),
-                      )
-                    ]
-                ),
-
-                child: ElevatedButton(
-                  onPressed:() {
-                    debugPrint('Go!');
-                  },
-                  style: ElevatedButton.styleFrom(
-                      padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)
-                      )
-                  ),
-                  child:const Text('Go!'),
-                )
-            ),
-
-            Row(children: [
-              Container (
-                  margin:const EdgeInsets.only(left: 16,right: 8),
-                  height: 50,
-                  width: 180,
-
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.08),
-                          offset:const Offset(0, 4),
-                        )
-                      ]
-                  ),
-
-                  child: ElevatedButton(
-                      onPressed:() {
-                        debugPrint('Stop the violence!');
-                      },
-                      style: ElevatedButton.styleFrom(
-                          padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.red,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)
-                          )
-                      ),
-                      child:const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.back_hand_outlined,color: Colors.white,),
-                            SizedBox(width: 10),
-                            Text(
-                                'Stop the violence',
-                                textAlign: TextAlign.center,
-                                maxLines: 2,
-                                style: TextStyle(color: Colors.white))
-                          ]
-                      )
-
-                  )
-              ),
-
-
-              Container (
-                  margin:const EdgeInsets.only(right: 16, left: 8),
-                  height: 50,
-                  width: 180,
-
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.08),
-                          offset:const Offset(0, 4),
-                        )
-                      ]
-                  ),
-
-                  child: ElevatedButton(
-                      onPressed:() {
-                        debugPrint('Edit!');
-                      },
-                      style: ElevatedButton.styleFrom(
-                          padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.lightBlueAccent,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)
-                          )
-                      ),
-                      child:const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(CupertinoIcons.plus,color: Colors.white,),
-                            SizedBox(width: 10),
-                            Text('Edit'),
-                            SizedBox(width: 10),
-                            Icon(CupertinoIcons.minus,color: Colors.white,)]
-                      )
-
-                  )
-              ),
-
-            ],
-            )
-          ]
-          )
-      ),
+      title: 'Flutter Screen',
+      home: HomeScreen(),
+      routes: {
+        '/logic1': (context)=> LogicScreen1(),
+        '/logic2': (context)=> LogicScreen2(),
+        '/logic3': (context)=> LogicScreen3(),
+        '/final': (context)=> FinalScreen(),
+      },
     );
-
-
   }
 }
 
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Главный экран'),
+      centerTitle: true,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: ()=> Navigator.pushNamed(context, '/logic1'),
+                child: Text('Экран 1')),
+            const SizedBox(height: 15.0),
+            ElevatedButton(
+                onPressed: ()=> Navigator.pushNamed(context, '/logic2'),
+                child: Text('Экран 2')),
+            const SizedBox(height: 15.0),
+            ElevatedButton(
+                onPressed: ()=> Navigator.pushNamed(context, '/logic3'),
+                child: Text('Экран 3')),
+            const SizedBox(height: 15.0),
+            ElevatedButton(
+                onPressed: ()=>Navigator.pushNamed(context, '/final'),
+                child: Text('Развязка')),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+
+class LogicScreen1 extends StatefulWidget {
+  @override
+  _LogicScreen1State createState() => _LogicScreen1State();
+}
+class _LogicScreen1State extends State<LogicScreen1>{
+  bool _isHidden = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Экран 1'),
+        centerTitle: true,),
+    body: Center(
+    child: _isHidden
+    ? Text('Hidden', style: TextStyle(fontSize: 25),)
+        :Image.network('https://filapp1.imgsmail.ru/pic?url=https%3A%2F%2Fotvet.imgsmail.ru%2Fdownload%2F179457519_6fb2312d2ffb76fa222914af5bdada71_800.jpg&sig=8af1813007d6b61593ac80b69d4346a8')
+    ),
+    bottomNavigationBar: Padding(
+    padding:const EdgeInsets.all(16.0),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+    Expanded(child: ElevatedButton(
+    onPressed: (){
+    setState(() {
+    _isHidden = true;
+    });
+    },
+    style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+    children: [Text('Скрыть', style: TextStyle(color: Colors.white,fontSize: 15)), Icon(CupertinoIcons.hand_point_left, color: Colors.white)],
+    )
+    )
+    ),
+
+    const SizedBox(width: 16,),
+
+    Expanded(child: ElevatedButton(
+    onPressed: (){
+    setState(() {
+    _isHidden = false;
+    });
+    },
+    style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text('Показать', style: TextStyle(color: Colors.yellow, fontSize: 15)), Icon(CupertinoIcons.hand_point_left_fill, color: Colors.yellow,)
+    ],
+    )
+
+    )
+    )
+    ],
+    ),
+    )
+    );
+  }
+}
+
+class LogicScreen2 extends StatefulWidget{
+  @override
+  _LogicScreen2State createState() => _LogicScreen2State();
+  }
+class _LogicScreen2State extends State<LogicScreen2>{
+  bool _isPresence = false;
+
+  Widget build(BuildContext contex) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Экран 2'),
+        centerTitle: true,
+      ),
+      body:Center(child: 
+        _isPresence
+          ? Image.network('https://avatars.mds.yandex.net/get-yapic/58107/cBmWI0WoDcFfgcqQEREiuFZ0SU-1/orig')
+            :Container(),
+      ),
+      bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(child:
+                ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        _isPresence = true;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.lightBlueAccent),
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Восстановить',style: TextStyle(color: Colors.green, fontSize: 15)),
+                    const SizedBox(width: 5),
+                    Icon(Icons.restore,color: Colors.green,)
+                  ],
+                )
+                )
+            ),
+
+            const SizedBox(width: 16),
+
+            Expanded(child:
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    _isPresence = false;
+                  });
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Удалить',style: TextStyle(color: Colors.red, fontSize: 15)),
+                    const SizedBox(width: 5),
+                    Icon(CupertinoIcons.delete, color: Colors.red)
+                  ],
+                )
+            )
+            ),
+
+          ],
+        ),
+      ),
+        
+    );
+  }
+}
+
+
+
+class LogicScreen3 extends StatefulWidget{
+  @override
+  _LogicScreen3State createState() => _LogicScreen3State();
+}
+
+class _LogicScreen3State extends State<LogicScreen3>{
+  double _opacity = 1.0;
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Экран 3'),
+        centerTitle: true,
+      ),
+      body: Center(child: Opacity(opacity: _opacity,
+      child: Image.network('https://avatars.mds.yandex.net/i?id=c47543b7dba408a70eb257673e8b6596b2b0ea57-5392430-images-thumbs&n=13')
+      )),
+      bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                child: ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        if (_opacity > 0.1) _opacity -= 0.1;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Уменьшить',style: TextStyle(color: Colors.black87, fontSize: 15)),
+                        const SizedBox(width: 5),
+                        Icon(CupertinoIcons.lightbulb, color: Colors.black87,)
+                      ],
+                    )
+                )
+            ),
+            const SizedBox(width: 16),
+
+            Expanded(
+                child: ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        if (_opacity< 1.0) _opacity += 0.1;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.black87),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Увеличить', style: TextStyle(color: Colors.blueGrey, fontSize: 15)),
+                        const SizedBox(width: 5),
+                        Icon(Icons.lightbulb, color: Colors.blueGrey,)
+                      ],
+                    )
+                ))
+
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class FinalScreen extends StatefulWidget{
+  @override
+  _FinalScreenState createState() => _FinalScreenState();
+}
+
+class _FinalScreenState extends State<FinalScreen>{
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Экран Развязки'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.network('https://a.d-cd.net/bQHHm3946rgr_qbCBJ4c6dSb2ng-960.jpg'),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// // Задание №5
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp (const MyApp());
+//
+//
+// class MyApp extends StatelessWidget{
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       title: "Three Button",
+//       theme: ThemeData.dark(),
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//           appBar: AppBar(
+//             title:const Text('___Button___'),
+//             centerTitle: true,
+//             titleTextStyle:const TextStyle(fontSize: 30,color:  Colors.blue),
+//           ),
+//
+//           body:Column(children: [
+//
+//             Container (
+//                 margin:const EdgeInsets.all(16),
+//                 height: 50,
+//                 width: 400,
+//
+//                 decoration: BoxDecoration(
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.white.withOpacity(0.08),
+//                         offset:const Offset(0, 4),
+//                       )
+//                     ]
+//                 ),
+//
+//                 child: ElevatedButton(
+//                   onPressed:() {
+//                     debugPrint('Go!');
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                       padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+//                       foregroundColor: Colors.white,
+//                       backgroundColor: Colors.green,
+//                       shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(15)
+//                       )
+//                   ),
+//                   child:const Text('Go!'),
+//                 )
+//             ),
+//
+//             Row(children: [
+//               Container (
+//                   margin:const EdgeInsets.only(left: 16,right: 8),
+//                   height: 50,
+//                   width: 180,
+//
+//                   decoration: BoxDecoration(
+//                       boxShadow: [
+//                         BoxShadow(
+//                           color: Colors.white.withOpacity(0.08),
+//                           offset:const Offset(0, 4),
+//                         )
+//                       ]
+//                   ),
+//
+//                   child: ElevatedButton(
+//                       onPressed:() {
+//                         debugPrint('Stop the violence!');
+//                       },
+//                       style: ElevatedButton.styleFrom(
+//                           padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+//                           foregroundColor: Colors.white,
+//                           backgroundColor: Colors.red,
+//                           shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(15)
+//                           )
+//                       ),
+//                       child:const Row(
+//                           mainAxisAlignment: MainAxisAlignment.center,
+//                           children: [
+//                             Icon(Icons.back_hand_outlined,color: Colors.white,),
+//                             SizedBox(width: 10),
+//                             Text(
+//                                 'Stop the violence',
+//                                 textAlign: TextAlign.center,
+//                                 maxLines: 2,
+//                                 style: TextStyle(color: Colors.white))
+//                           ]
+//                       )
+//
+//                   )
+//               ),
+//
+//
+//               Container (
+//                   margin:const EdgeInsets.only(right: 16, left: 8),
+//                   height: 50,
+//                   width: 180,
+//
+//                   decoration: BoxDecoration(
+//                       boxShadow: [
+//                         BoxShadow(
+//                           color: Colors.white.withOpacity(0.08),
+//                           offset:const Offset(0, 4),
+//                         )
+//                       ]
+//                   ),
+//
+//                   child: ElevatedButton(
+//                       onPressed:() {
+//                         debugPrint('Edit!');
+//                       },
+//                       style: ElevatedButton.styleFrom(
+//                           padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+//                           foregroundColor: Colors.white,
+//                           backgroundColor: Colors.lightBlueAccent,
+//                           shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(15)
+//                           )
+//                       ),
+//                       child:const Row(
+//                           mainAxisAlignment: MainAxisAlignment.center,
+//                           children: [
+//                             Icon(CupertinoIcons.plus,color: Colors.white,),
+//                             SizedBox(width: 10),
+//                             Text('Edit'),
+//                             SizedBox(width: 10),
+//                             Icon(CupertinoIcons.minus,color: Colors.white,)]
+//                       )
+//
+//                   )
+//               ),
+//
+//             ],
+//             )
+//           ]
+//           )
+//       ),
+//     );
+//
+//
+//   }
+// }
+//
 
 
 
